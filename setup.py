e5ac1d55f08fc2944a02ec0d327b3c9bf6aa231c
Thomas Levine <_@thomaslevine.com>
2015-04-26 00:18:30 +0000

    copy from a django project and adapt

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+from shrebx import shrebx
+
+setup(name='shrebx',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description=shrebx.__doc__,
+      url='https://github.com/tlevine/acpr-banque-de-france',
+      py_modules=['shrebx'],
+      version='0.0.1',
+      license='AGPL',
+)
---
+++ shrebx.py
@@ -0,0 +1,34 @@
+import os
+from random import sample
+from string import ascii_letters, digits
+from configparser import ConfigParser, NoOptionError
+
+def shrebx(BASE_DIR, file_name = '.secrets', section_name = 'secrets',
+    random_fields = ['DATABASE_PASSWORD'], manual_fields = ['SECRET_KEY'],
+    key_length = 62):
+    'Read and write secrets from a configuration file.'
+
+    config_file = os.path.join(BASE_DIR, file_name)
+    c = ConfigParser()
+    c.read(config_file)
+    if section_name not in c.sections():
+        c.add_section(section_name)
+    section = c[section_name]
+
+    output = {}
+    for manual_field in manual_fields:
+        if manual_field in section.keys():
+            output[manual_field] = section[manual_field]
+        else:
+            output[manual_field] = None
+
+    for random_field in random_fields:
+        if random_field not in section.keys():
+            key = ''.join(sample(ascii_letters + digits, key_length))
+            c.set(section_name, random_field, key)
+        output[random_field] = c.get(section_name, random_field)
+
+    with open(config_file, 'w') as fp:
+        c.write(fp)
+
+    return output
